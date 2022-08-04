3ea7788c227b68373a84c8b2d947d63fc652eb35
AntonioQuatrale93 <103583055+AntonioQuatrale93@users.noreply.github.com>
2022-08-04 16:57:34 +0200

    Add files via upload

---
+++ EnvironmentVariable02/src/main/java/com/example/EnvironmentVariable02/EnvironmentVariable02Application.java
@@ -0,0 +1,13 @@
+package com.example.EnvironmentVariable02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentVariable02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentVariable02Application.class, args);
+	}
+
+}
